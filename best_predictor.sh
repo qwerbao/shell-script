0137b30de52e5f71beb2e54ec26632b010eddc63
jxingchen2@gmail.com <jxingchen2@gmail.com>
2024-05-20 10:16:32 +0100

    Add function to check if the input file exists

--- best_predictor.sh
+++ best_predictor.sh
@@ -5,3 +5,14 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+# Function to check if the file exists
+check_file_exist() {
+    for file in "$@"; do
+        if [ ! -f "$file" ]; then
+            echo "Input file $file does not exist."
+            exit 1
+        fi
+    done
+}
+
+
